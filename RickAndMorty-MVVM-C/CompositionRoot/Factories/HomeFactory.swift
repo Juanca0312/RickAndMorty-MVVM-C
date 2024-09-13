27dd178c1e6ac0585309385920948b6cf94d40e8
Juan Carlos Hernandez Castillo <jchernandez0312@gmail.com>
2024-09-13 17:54:18 -0500

    Configure HomeMenuController collection view with cell

--- RickAndMorty-MVVM-C/CompositionRoot/Factories/HomeFactory.swift
+++ RickAndMorty-MVVM-C/CompositionRoot/Factories/HomeFactory.swift
@@ -13,8 +13,20 @@ protocol HomeFactory {
 
 struct HomeFactoryImpl: HomeFactory {
     func makeModule() -> UIViewController {
-        let homeMenuController = HomeMenuController()
+        
+        let homeMenuController = HomeMenuController(collectionViewLayout: makeLayout())
         homeMenuController.title = "Rick and Morty App"
         return homeMenuController
+    }
+    
+    private func makeLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let layoutWidth = (UIScreen.main.bounds.width - 20) / 2
+        let layoutHeight = (UIScreen.main.bounds.width - 20)  / 2
+        layout.itemSize = CGSize(width: layoutWidth, height: layoutHeight)
+        layout.minimumLineSpacing = .zero
+        layout.minimumInteritemSpacing = .zero
+        layout.sectionInset = UIEdgeInsets(top: .zero, left: 10, bottom: .zero, right: 10)
+        return layout
     }
 }
--- RickAndMorty-MVVM-C/ScreenModules/HomeMenu/HomeMenuController.swift
+++ RickAndMorty-MVVM-C/ScreenModules/HomeMenu/HomeMenuController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class HomeMenuController: UIViewController {
+final class HomeMenuController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupCollectionView()
+    }
+    
+    private func setupUI() {
         view.backgroundColor = .systemBackground
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(ItemHomeMenuCell.self, forCellWithReuseIdentifier: ItemHomeMenuCell.reuseIdentifier)
     }
 
 
 }
 
+
+extension HomeMenuController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemHomeMenuCell.reuseIdentifier, for: indexPath) as? ItemHomeMenuCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
