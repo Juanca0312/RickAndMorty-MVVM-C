3e325cce7b25fbf2be8c5d2a03bf4e94e6bc3766
Juan Carlos Hernandez Castillo <jchernandez0312@gmail.com>
2024-09-13 16:42:49 -0500

    Initial project configuration
    - Removed Storyboards
    - Add Coordinator protocol
    - Add AppFactory and AppCoordinator
    - Add HomeFactory, HomeCoordinator and HomeMenuController

---
+++ RickAndMorty-MVVM-C/ScreenModules/HomeMenu/HomeMenuController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeMenuController.swift
+//  RickAndMorty-MVVM-C
+//
+//  Created by Juan Carlos Hernandez Castillo on 13/09/24.
+//
+
+import UIKit
+
+class HomeMenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
