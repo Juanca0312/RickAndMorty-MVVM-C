56057909eb93224bd767773cd89c779523ecc73d
Juan Carlos Hernandez Castillo <jchernandez0312@gmail.com>
2024-09-13 23:18:04 -0500

    Add MenuItem entity

---
+++ RickAndMorty-MVVM-C/ScreenModules/HomeMenu/Domain/Entities/MenuItem.swift
@@ -0,0 +1,13 @@
+//
+//  MenuItem.swift
+//  RickAndMorty-MVVM-C
+//
+//  Created by Juan Carlos Hernandez Castillo on 13/09/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    let title: String
+    let url: String
+}
